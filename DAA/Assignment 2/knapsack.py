34426a3adea3230555d9982458cf3490edae0b9e
Prathamesh <prathamesh.bhosale.0201@gmail.com>
2022-08-19 12:31:43 +0530

    knapsack implemented

--- DAA/Assignment 2/knapsack.py
+++ DAA/Assignment 2/knapsack.py
@@ -1,13 +1,23 @@
 items = []
+value = 0
 capacity = float(input("Enter Bag capacity: ")) 
 n = int(input("Number of Items: "))
 
 for i in range(n):
-    name = input("Enter item name: ")
-    profit = float(input("Enter profit: "))
-    weight = float(input("Enter weight: "))
+    name, profit, weight = input("Enter item name, profit and weight: ").split(" ")
+
+    profit = float(profit)
+    weight = float(weight)
     pwr = float(profit/weight)
     items.append([name,profit,weight,pwr])
 
 items.sort(key=lambda a:a[3],reverse=True)
 
+for i in items:
+    if i[2] <= capacity:
+        capacity -= i[2]
+        value += i[1]
+    else:
+        value += capacity*i[3]
+        break
+print("Total Profit: ",value)
