762ef59986d7b1c0220671f328dd426eb2681534
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2022-11-10 19:13:48 +0530

    .

--- DAA/Assignment 5 N-Queen/Queens.java
+++ DAA/Assignment 5 N-Queen/Queens.java
@@ -1,9 +1,6 @@
 public class Queens {
 
-    /***************************************************************************
-     * Return true if queen placement q[n] does not conflict with
-     * other queens q[0] through q[n-1]
-     ***************************************************************************/
+
      public static boolean isConsistent(int[] q, int n) {
          for (int i = 0; i < n; i++) {
              if (q[i] == q[n])             return false;   // same column
@@ -13,9 +10,7 @@ public class Queens {
          return true;
      }
  
-    /***************************************************************************
-     * Prints n-by-n placement of queens from permutation q in ASCII.
-     ***************************************************************************/
+
      public static void printQueens(int[] q) {
          int n = q.length;
          for (int i = 0; i < n; i++) {
@@ -29,9 +24,6 @@ public class Queens {
      }
  
  
-    /***************************************************************************
-     *  Try all permutations using backtracking
-     ***************************************************************************/
      public static void enumerate(int n) {
          int[] a = new int[n];
          enumerate(a, 0);
